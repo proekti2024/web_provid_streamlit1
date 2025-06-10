b71401ecd5ce5d5d54cd87ae154a8e77c2b9a9e7
proekti2024 <proektii.2024@gmail.com>
2025-06-10 20:56:31 +0200

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def scrape_books(num_pages=3):
+    base_url = "http://books.toscrape.com/catalogue/page-{}.html"
+    titles, prices, ratings = [], [], []
+
+    for page in range(1, num_pages + 1):
+        url = base_url.format(page)
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        books = soup.find_all('article', class_='product_pod')
+
+        for book in books:
+            title = book.h3.a['title']
+            price = float(book.find('p', class_='price_color').text[1:])
+            rating = book.p['class'][1]
+
+            titles.append(title)
+            prices.append(price)
+            ratings.append(rating)
+
+    df = pd.DataFrame({
+        'Title': titles,
+        'Price (£)': prices,
+        'Rating': ratings
+    })
+    return df
